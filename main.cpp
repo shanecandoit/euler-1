4b677d1e18d3e497d258bbd0bd087faea3240842
outOfBounds <shanecandoit@gmail.com>
2011-12-19 11:27:08 -0600

    summing logic was bunk, fixed

--- main.cpp
+++ main.cpp
@@ -5,16 +5,21 @@ int main()
 {
     unsigned long sum=0;
     for( long num=1; num<1000; ++num)
+    //for( long num=1; num<10; ++num)
     {
         if(num%3==0)
         {
             //
-            sum+=3;
+            sum+=num;
+            //printf("num:%d \n",num);
+            //printf("sum:%d \n",sum);
         }
         else if(num%5==0)
         {
             //
-            sum+=5;
+            sum+=num;
+            //printf("num:%d \n",num);
+            //printf("sum:%d \n",sum);
         }
         //printf("sum: %d \n",sum);
     }
